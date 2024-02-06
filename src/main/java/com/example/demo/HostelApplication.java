9a11b9c7f570b8b23b7d4512ce8925399dc40bf0
Dhanush <151453741+KickButtowski33@users.noreply.github.com>
2024-02-06 17:51:18 +0530

    Update HostelApplication.java

--- src/main/java/com/example/demo/HostelApplication.java
+++ src/main/java/com/example/demo/HostelApplication.java
@@ -8,12 +8,6 @@ public class HostelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HostelApplication.class, args);
-
-		System.out.println("hello");
-
-		System.Out.Print("hello");
-		System.Out.Print("hello");
-		System.Out.Print("hello");
 		//this is the fist change
 
 	}
